5ab6b2b213501e00c13dc233ed10635d9f660c09
arunsvaddi <arunsvaddi@gmail.com>
2024-05-28 19:00:10 +0530

    connected to data base and mongoose

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
 import express from 'express';
 
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { error } from 'console';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+   console.log("connected sucessfully to MongoDB ")
+}).catch((error)=>{
+   console.log(error)
+})
+
 const app=express();
 
+
+
 app.listen(3000,()=>{
+
    console.log("server connected  at port 30000")
+
 }
 );
